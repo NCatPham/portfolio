f79b7db144ffeddbcec9ab64cf1132ee5035c837
Nicholas Pham <nicholascatpham@gmail.com>
2024-04-30 12:09:06 -0700

    editted app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +18,26 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
+    <body>
+      <section id="navigationbar">
+          <nav class="navbar navbar-expand-lg navbar-dark navMargin">
+              <a class="navbar-brand" href="index.html">
+                  <img src="images/NP_Logo.png" width="84" height="60" class="d-inline-block align-top" alt="Logo"></img>
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav ml-auto">
+                      <li class="nav-item">
+                          <a class="nav-link" href="PhamNicholasResume.pdf">Resume</a>
+                      </li>
+                  </ul>
+              </div>
+          </nav>
+      </section>
+    </body>
   );
 }
 
